9ced5021adb646b02c94d36e21c008aeb08d4514
ArismarSantiago <arismar133@gmail.com>
2026-07-02 17:51:11 -0400

    OrderStatus enum

--- src/main/java/myproject/projectCourse/entities/Order.java
+++ src/main/java/myproject/projectCourse/entities/Order.java
@@ -13,6 +13,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import myproject.projectCourse.entities.enums.OrderStatus;
 
 @Entity
 @Table(name = "tb_order")
@@ -25,7 +26,9 @@ public class Order implements Serializable {
 
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-mm-dd'T'HH'mm'ss'Z", timezone = "GMT")
 	private Instant moment;
-	// private OrderStatus orderStatus;
+	
+	
+	private Integer orderStatus;
 
 	@ManyToOne
 	@JoinColumn(name = "client_id")
@@ -35,10 +38,11 @@ public class Order implements Serializable {
 
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -64,6 +68,17 @@ public class Order implements Serializable {
 
 	public void setClient(User client) {
 		this.client = client;
+	}
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		
+		if(orderStatus != null) {
+		this.orderStatus = orderStatus.getCode();
+		}
 	}
 
 	@Override
